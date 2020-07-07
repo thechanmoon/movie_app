c6303248fab2d3d62a10429c3ea8dc27b4d7e64a
chan <thechanmoon@gmail.com>
2020-07-07 17:25:09 -0400

    Component Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Watermelon from './Watermelon'
-const cube = [1728, 729];
+// import Watermelon from './Watermelon'
+// const cube = [1728, 729];
 
 // const cubeRoot = () => 
 // {
@@ -9,13 +9,48 @@ const cube = [1728, 729];
 //     '<div>{element}</div>`
 //   }
 // };
+const fruits = 
+  [
+   {
+     name:"Watermelon", 
+     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Watermelon_seedless.jpg/220px-Watermelon_seedless.jpg"
+   },
+   {
+    name:"Grape", 
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Table_grapes_on_white.jpg/220px-Table_grapes_on_white.jpg"
+   }
+  ]
 
+// const fruits = ["water melon", "grape"]
+
+function Fruit({name,image}){
+return (
+  <div>
+    <h3>{name} is my favorite fruit  !!</h3>
+    <img src={image} alt={name}/>
+  </div>
+  )}
 
 function App() {
+  let index = 0;
   return (
-  // <div>JW!! Plz Focus on your math. {Math.cbrt(cube[0])}</div>
-  <Watermelon> </Watermelon>
+  <div>
+  {
+    
+  fruits.map( function (fruit){
+    return <Fruit key = {index++} name = {fruit.name} image = {fruit.image}> </Fruit>
+  })
+  }
+  </div>
   );
 }
+
+{/* <div className="someDiv">
+{
+  collection.map(function (item) {
+    return <div> {item.Name} {this.getItemInfo(item)} </div>
+})
+}
+</div> */}
 
 export default App;
--- src/Watermelon.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Watermelon()
-{
-    return (
-        <div>Watermelon</div>
-    )
-}
