6145c07a0033a29ac0d3288181237e1e66967908
chan <thechanmoon@gmail.com>
2020-07-08 00:05:05 -0400

    Button Added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,34 @@ import React from 'react';
 
 class App extends React.Component
 {
+  state = {
+    number: 0
+  }
+  
+  addNumber = () =>
+  {
+    // this.setState({number: this.state.number+1})
+    this.setState(prevState=>({number: prevState.number+1}))
+  }
+
+  subtractNumber = () =>
+  {
+    // this.setState({number: this.state.number-1})
+    this.setState(prevState=>({number: prevState.number-1}))
+  }
+
   render()
   {
-    return <div>Class Component</div>
+    return (<div>
+      
+      <h1> The Number is: {this.state.number} </h1>
+      {/* <button onClick = {()=>this.setState( {number: this.state.number+1})}>Add</button>
+      <button onClick = {()=>this.setState( {number: this.state.number-1})}>Subtract</button> */}
+      <button onClick = {this.addNumber}>Add</button>
+      <button onClick = {this.subtractNumber}>Subtract</button>
+      
+    </div>)
   }
 }
 export default App;
+ 
