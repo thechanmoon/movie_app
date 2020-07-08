9d0b0fa7874d51af609829255b7892dce4d56559
chan <thechanmoon@gmail.com>
2020-07-07 21:58:08 -0400

    Component Test Comlpleted

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
 import React from 'react';
-// import Watermelon from './Watermelon'
-// const cube = [1728, 729];
+import PropTypes from 'prop-types';
 
-// const cubeRoot = () => 
-// {
-//   for (let index = 0; index < cube.length; index++) {
-//     const element = cube[index];
-//     '<div>{element}</div>`
-//   }
-// };
 const fruits = 
   [
    {
-     name:"Watermelon", 
-     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Watermelon_seedless.jpg/220px-Watermelon_seedless.jpg"
-   },
+    id: 0, 
+    name:"Watermelon", 
+     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Watermelon_seedless.jpg/220px-Watermelon_seedless.jpg",
+    likes: 5
+    },
    {
+    id: 1,
     name:"Grape", 
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Table_grapes_on_white.jpg/220px-Table_grapes_on_white.jpg"
-   }
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Table_grapes_on_white.jpg/220px-Table_grapes_on_white.jpg",
+    likes: 4
+  }
   ]
 
+  fruits.propTypes = 
+  {
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired
+  }
 // const fruits = ["water melon", "grape"]
 
-function Fruit({name,image}){
+function Fruit({name,image, likes}){
 return (
   <div>
     <h3>{name} is my favorite fruit  !!</h3>
+    <h2>{likes}/5.0</h2>
     <img src={image} alt={name}/>
   </div>
   )}
 
 function App() {
-  let index = 0;
+  // let index = 0;
   return (
   <div>
   {
     
-  fruits.map( function (fruit){
-    return <Fruit key = {index++} name = {fruit.name} image = {fruit.image}> </Fruit>
+  fruits.map( fruit => {
+    // return <Fruit key = {index++} name = {fruit.name} image = {fruit.image}> </Fruit>
+    return <Fruit key = {fruit.id} name = {fruit.name} image = {fruit.image} likes = {fruit.likes}> </Fruit>
   })
   }
   </div>
   );
 }
-
-{/* <div className="someDiv">
-{
-  collection.map(function (item) {
-    return <div> {item.Name} {this.getItemInfo(item)} </div>
-})
-}
-</div> */}
 
 export default App;
