23d88ec06722823acc017bf5896db2f0bdf18105
chan <thechanmoon@gmail.com>
2020-07-21 01:56:40 -0400

    Updates

---
+++ precache-manifest.28ecbd60247befc0522ba2353f27c4e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13250a3f3842a4e0912788bfce802cb6",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "62405ffa8a7e8e99ec44",
+    "url": "/movie_app/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62405ffa8a7e8e99ec44",
+    "url": "/movie_app/static/js/main.86aa9178.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
